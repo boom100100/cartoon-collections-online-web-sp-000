e4d994d9b494ffa77b2ecb40a47ef8a8456003e1
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 20:21:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,15 +17,18 @@ end
 
 def long_planeteer_calls(arrayOfCalls)# code an argument here
   # Your code here
-
-
   arrayOfCalls.any? {|call|
   call.length > 4
 }
 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each {|element|
+    return element if cheese_types.any?{|type|
+      type == element
+    }
+  }
 end
